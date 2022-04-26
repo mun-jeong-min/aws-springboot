38a3826e2cd4b7cbc46069e2822c5cc612f7d01c
munjeongmin <3330kys@dsm.hs.kr>
2022-04-26 16:01:49 +0900

    [ADD] aws 실행용 controller 추가

---
+++ src/main/java/com/example/aws/Controller.java
@@ -0,0 +1,14 @@
+package com.example.aws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stop")
+public class Controller {
+    @GetMapping("/as")
+    public String get() {
+        return "liverpool";
+    }
+}

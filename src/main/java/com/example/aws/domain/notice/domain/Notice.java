24a39a678f38fb352704027cc011f32319adc6d8
munjeongmin <3330kys@dsm.hs.kr>
2022-04-27 17:26:03 +0900

    [ADD] notice entity 추가

---
+++ src/main/java/com/example/aws/domain/notice/domain/Notice.java
@@ -0,0 +1,30 @@
+package com.example.aws.domain.notice.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Builder
+    public Notice(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}

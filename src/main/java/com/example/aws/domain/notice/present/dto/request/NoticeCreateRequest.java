05d0ce53aed1a41b47181650bffca336f21d403d
munjeongmin <3330kys@dsm.hs.kr>
2022-04-27 17:27:36 +0900

    [ADD] noticeCreateRequest 추가

---
+++ src/main/java/com/example/aws/domain/notice/present/dto/request/NoticeCreateRequest.java
@@ -0,0 +1,11 @@
+package com.example.aws.domain.notice.present.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class NoticeCreateRequest {
+    private String title;
+    private String content;
+}

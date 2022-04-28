742c088b3efe0d4e5c81c5822add8b6de8d4dd8f
munjeongmin <3330kys@dsm.hs.kr>
2022-04-28 11:02:11 +0900

    [ADD] SecurityConfig 추가

---
+++ src/main/java/com/example/aws/global/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.aws.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .formLogin().disable()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+
+                .antMatchers(HttpMethod.POST, "/notice/post").permitAll()
+                .antMatchers(HttpMethod.PUT, "/notice/put/{id}").permitAll()
+
+                .anyRequest().authenticated();
+    }
+}

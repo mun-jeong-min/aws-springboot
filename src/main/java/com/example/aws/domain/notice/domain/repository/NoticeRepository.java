e3d529cdbd44489956e3509f376cbe5a852a70b9
munjeongmin <3330kys@dsm.hs.kr>
2022-04-28 11:00:08 +0900

    [ADD] NoticeUpdateRequestDto 추가

--- src/main/java/com/example/aws/domain/notice/domain/repository/NoticeRepository.java
+++ src/main/java/com/example/aws/domain/notice/domain/repository/NoticeRepository.java
@@ -3,5 +3,8 @@ package com.example.aws.domain.notice.domain.repository;
 import com.example.aws.domain.notice.domain.Notice;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface NoticeRepository extends CrudRepository<Notice, Long> {
+    Optional<Notice> findNoticeById(Long id);
 }

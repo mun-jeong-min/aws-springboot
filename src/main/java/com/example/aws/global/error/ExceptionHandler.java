c75ee669052a79afc03e3f0a5984244e05717ded
munjeongmin <3330kys@dsm.hs.kr>
2022-04-28 11:01:37 +0900

    [ADD] exceptionHandler 추가

---
+++ src/main/java/com/example/aws/global/error/ExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.aws.global.error;
+
+import com.example.aws.global.error.exception.ProjectException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ExceptionHandler extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (ProjectException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getMessage());
+
+            response.setStatus(e.getErrorCode().getStatus());
+            response.setContentType("application/json");
+            response.getWriter().write(errorResponse.convertToJson(errorResponse));
+        }
+    }
+}

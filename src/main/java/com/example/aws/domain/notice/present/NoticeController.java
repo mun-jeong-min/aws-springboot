f0035ac77f5dec39dddf30966d45ba825e7abc3d
munjeongmin <3330kys@dsm.hs.kr>
2022-04-28 11:02:33 +0900

    [EDIT] NoticeUpdateController 추가

--- src/main/java/com/example/aws/domain/notice/present/NoticeController.java
+++ src/main/java/com/example/aws/domain/notice/present/NoticeController.java
@@ -1,6 +1,7 @@
 package com.example.aws.domain.notice.present;
 
 import com.example.aws.domain.notice.present.dto.request.NoticeCreateRequest;
+import com.example.aws.domain.notice.present.dto.request.NoticeUpdateRequest;
 import com.example.aws.domain.notice.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,9 +13,15 @@ import org.springframework.web.bind.annotation.*;
 public class NoticeController {
     private final NoticeService noticeService;
 
-    @PostMapping("/")
+    @PostMapping("/post")
     @ResponseStatus(HttpStatus.CREATED)
     public void noticeCreate(@RequestBody NoticeCreateRequest request) {
         noticeService.noticeCreate(request);
+    }
+
+    @PutMapping("/put/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void noticeUpdate(@PathVariable("id") Long id, @RequestBody NoticeUpdateRequest request) {
+        noticeService.updateNotice(id, request);
     }
 }

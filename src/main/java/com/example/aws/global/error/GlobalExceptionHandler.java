4dc15c485fc2d8d55e619136e8b5b41f4c6d2493
munjeongmin <3330kys@dsm.hs.kr>
2022-04-28 22:35:59 +0900

    [ADD] GlobalExceptionHandler 추가

---
+++ src/main/java/com/example/aws/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.aws.global.error;
+
+import com.example.aws.global.error.exception.ErrorCode;
+import com.example.aws.global.error.exception.ProjectException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProjectException.class)
+    public ResponseEntity<ErrorResponse> handleException(ProjectException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        return new ResponseEntity<>(new ErrorResponse(errorCode.getMessage()), HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleVaildException(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(
+                new ErrorResponse(e.getBindingResult().getAllErrors().get(0).getDefaultMessage()), HttpStatus.BAD_REQUEST
+        );
+    }
+}

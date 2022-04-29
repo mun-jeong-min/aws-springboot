4baba059f2eec6e381b027aea40ceea20114ba3d
munjeongmin <3330kys@dsm.hs.kr>
2022-04-29 15:48:21 +0900

    [EDIT] NoticeController에 delete,read 추가

--- src/main/java/com/example/aws/domain/notice/present/NoticeController.java
+++ src/main/java/com/example/aws/domain/notice/present/NoticeController.java
@@ -1,11 +1,14 @@
 package com.example.aws.domain.notice.present;
 
+import com.example.aws.domain.notice.domain.Notice;
 import com.example.aws.domain.notice.present.dto.request.NoticeCreateRequest;
 import com.example.aws.domain.notice.present.dto.request.NoticeUpdateRequest;
 import com.example.aws.domain.notice.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +26,16 @@ public class NoticeController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void noticeUpdate(@PathVariable("id") Long id, @RequestBody NoticeUpdateRequest request) {
         noticeService.updateNotice(id, request);
+    }
+
+    @GetMapping("/get")
+    public List<Notice> noticeRead() {
+        return noticeService.noticeRead();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void noticeDelete(@PathVariable("id") Long id) {
+        noticeService.deleteNotice(id);
     }
 }

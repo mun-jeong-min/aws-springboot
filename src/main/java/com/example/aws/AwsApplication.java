e8dd814166f813d487511c0e37a188ab74b3f480
munjeongmin <3330kys@dsm.hs.kr>
2022-05-05 22:44:54 +0900

    [ADD] JwtProperties 추가

--- src/main/java/com/example/aws/AwsApplication.java
+++ src/main/java/com/example/aws/AwsApplication.java
@@ -2,9 +2,12 @@ package com.example.aws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AwsApplication {
 
     public static void main(String[] args) {

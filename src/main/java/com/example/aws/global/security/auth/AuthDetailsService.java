9c9c1e2310f2eaf8c373a6fbcb6a7ff1a0a467f4
munjeongmin <3330kys@dsm.hs.kr>
2022-05-06 19:50:57 +0900

    [ADD] authDetailsService 추가

---
+++ src/main/java/com/example/aws/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,16 @@
+package com.example.aws.global.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}

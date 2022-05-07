98bf3f1cbd6b0b41bb486d9f781c10cb2990bd17
munjeongmin <3330kys@dsm.hs.kr>
2022-05-08 02:13:01 +0900

    [ADD] RefreshToken domain 추가

---
+++ src/main/java/com/example/aws/domain/auth/domain/RefreshToken.java
@@ -0,0 +1,25 @@
+package com.example.aws.domain.auth.domain;
+
+import lombok.*;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+import javax.persistence.Id;
+
+@Builder
+@Getter
+@RedisHash
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class RefreshToken {
+
+    @Id
+    private String accountId;
+
+    @Indexed
+    private String token;
+
+    @TimeToLive
+    private Long timeToLive;
+}

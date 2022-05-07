afe75851ff9c8b1d95b82353fd51237947d67f35
munjeongmin <3330kys@dsm.hs.kr>
2022-05-07 21:40:12 +0900

    [ADD] service, controller 추가

---
+++ src/main/java/com/example/aws/domain/user/present/UserController.java
@@ -0,0 +1,11 @@
+package com.example.aws.domain.user.present;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+}
---
+++ src/main/java/com/example/aws/domain/user/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.aws.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserService userService;
+}

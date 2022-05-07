168feaf7c6147bd0284fe24585cb0b0b7d7bfcaa
munjeongmin <3330kys@dsm.hs.kr>
2022-05-08 01:28:08 +0900

    [EDIT] controller에 signup 추가

--- src/main/java/com/example/aws/domain/user/present/UserController.java
+++ src/main/java/com/example/aws/domain/user/present/UserController.java
@@ -1,11 +1,21 @@
 package com.example.aws.domain.user.present;
 
+import com.example.aws.domain.user.present.dto.request.SignupRequest;
+import com.example.aws.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void signup(@RequestBody SignupRequest request) {
+        userService.signup(request);
+    }
 }

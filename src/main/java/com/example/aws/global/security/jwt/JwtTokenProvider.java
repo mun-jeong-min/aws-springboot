5c8756aa7991700f4168cfe3af606a1968a0e703
munjeongmin <3330kys@dsm.hs.kr>
2022-05-09 14:39:37 +0900

    [ADD] jwtTokenProvider 추가

---
+++ src/main/java/com/example/aws/global/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,78 @@
+package com.example.aws.global.security.jwt;
+
+import com.example.aws.domain.auth.domain.RefreshToken;
+import com.example.aws.domain.auth.domain.repository.RefreshTokenRepository;
+import com.example.aws.global.security.auth.AuthDetailsService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private final JwtProperties jwtProperties;
+    private final AuthDetailsService authDetailsService;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public String generateAccessToken(String id) {
+        return generateToken(id, "access", jwtProperties.getAccessExp());
+    }
+
+    public String generateRefreshToken(String id) {
+        String refreshToken = generateToken(id, "refresh", jwtProperties.getRefreshExp());
+
+        refreshTokenRepository.save(RefreshToken.builder()
+                .accountId(id)
+                .token(refreshToken)
+                .timeToLive(jwtProperties.getRefreshExp())
+                .build());
+
+        return refreshToken;
+    }
+
+    private String generateToken(String id, String type, Long exp) {
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .setSubject(id)
+                .claim("type", type)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + exp * 1000))
+                .compact();
+    }
+
+    public String parseToken(String bearerToken) {
+        if(bearerToken != null && bearerToken.startsWith(jwtProperties.getPrefix())) {
+            return bearerToken.replace(jwtProperties.getPrefix(), "");
+        }
+        return null;
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearer = request.getHeader(jwtProperties.getHeader());
+        return parseToken(bearer);
+    }
+
+    private Claims getTokenBody(String token) {
+        return Jwts.parser().setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token).getBody();
+    }
+
+    private String getTokenSubject(String token) {
+        return getTokenBody(token).getSubject();
+    }
+
+    public Authentication authentication(String token) {
+        UserDetails userDetails = authDetailsService
+                .loadUserByUsername(getTokenSubject(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+}
